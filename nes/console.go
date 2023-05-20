41dfe2bb6e9d740dba86248a8335d23a8e120217
bytes <therealbyt3s@gmail.com>
2023-05-20 14:09:06 +0200

    feat: disable apu

--- nes/console.go
+++ nes/console.go
@@ -52,9 +52,9 @@ func (console *Console) Step() int {
 		console.PPU.Step()
 		console.Mapper.Step()
 	}
-	for i := 0; i < cpuCycles; i++ {
-		console.APU.Step()
-	}
+	// for i := 0; i < cpuCycles; i++ {
+	// 	console.APU.Step()
+	// }
 	return cpuCycles
 }
 

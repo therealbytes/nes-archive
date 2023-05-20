bf3e13b1d4b04cbcade323a8dc0733b00cfa2e7a
bytes <therealbyt3s@gmail.com>
2023-05-20 13:51:11 +0200

    feat: clean, kb

--- wasm/main.go
+++ wasm/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	_ "embed"
+	"fmt"
 	"syscall/js"
 	"time"
 
@@ -20,7 +21,7 @@ const (
 )
 
 func main() {
-	// kb := NewKeyboard()
+	kb := NewKeyboard()
 	renderer := NewRenderer()
 
 	nes, err := nes.NewHeadlessConsole(staticData, dynamicData)
@@ -28,42 +29,57 @@ func main() {
 		panic(err)
 	}
 
-	spf := time.Second / 10
+	spf := time.Second / 60
 	ticker := time.NewTicker(spf)
 	defer ticker.Stop()
 
 	for range ticker.C {
+		controller := kb.getController()
+		nes.Controller1.SetButtons(controller)
+		startTime := time.Now()
 		nes.StepSeconds(spf.Seconds())
+		fmt.Println("[wasm] Stepped", spf, "in", time.Since(startTime))
+		startTime = time.Now()
 		renderer.renderImage(nes.Buffer().Pix)
+		fmt.Println("[wasm] Renderer in", time.Since(startTime))
 	}
 }
 
-type renderer struct{}
-
-func NewRenderer() *renderer {
-	return &renderer{}
+type renderer struct {
+	document  js.Value
+	canvas    js.Value
+	context   js.Value
+	imageData js.Value
+	jsData    js.Value
 }
 
-func (r *renderer) renderImage(rawImageData []uint8) {
+func NewRenderer() *renderer {
 	document := js.Global().Get("document")
 	canvas := document.Call("getElementById", "canvas")
 	context := canvas.Call("getContext", "2d")
 	imageData := context.Call("createImageData", NES_WIDTH, NES_HEIGHT)
 	jsData := imageData.Get("data")
-	js.CopyBytesToJS(jsData, rawImageData)
-	context.Call("putImageData", imageData, 0, 0)
+	return &renderer{
+		document:  document,
+		canvas:    canvas,
+		context:   context,
+		imageData: imageData,
+		jsData:    jsData,
+	}
+}
+
+func (r *renderer) renderImage(rawImageData []uint8) {
+	r.context.Call("clearRect", 0, 0, r.canvas.Get("width").Int(), r.canvas.Get("height").Int())
+	js.CopyBytesToJS(r.jsData, rawImageData)
+	r.context.Call("putImageData", r.imageData, 0, 0)
 }
 
 type keyboard struct {
-	keyStates     map[string]bool
-	pendingKeyUps map[string]bool
+	keyStates map[string]bool
 }
 
 func NewKeyboard() *keyboard {
-	kb := &keyboard{
-		keyStates:     make(map[string]bool),
-		pendingKeyUps: make(map[string]bool),
-	}
+	kb := &keyboard{make(map[string]bool)}
 	window := js.Global().Get("window")
 	window.Call("addEventListener", "keydown", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		event := args[0]
@@ -74,16 +90,10 @@ func NewKeyboard() *keyboard {
 	window.Call("addEventListener", "keyup", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
 		event := args[0]
 		key := event.Get("code").String()
-		kb.pendingKeyUps[key] = false
+		kb.keyStates[key] = false
 		return nil
 	}))
 	return kb
-}
-
-func (kb *keyboard) finaliseKeyUps() {
-	for key, value := range kb.pendingKeyUps {
-		kb.keyStates[key] = value
-	}
 }
 
 func (kb *keyboard) getController() [8]bool {

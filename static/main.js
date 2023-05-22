5318b2d329b5aa41e8f3bea645ad0c0f8dec13a5
bytes <therealbyt3s@gmail.com>
2023-05-22 13:01:25 +0200

    feat: pi cache, controller, governor

--- static/main.js
+++ static/main.js
@@ -1,3 +1,21 @@
+function hexToUint8Array(hexString) {
+    if (hexString.length % 2 !== 0) {
+        console.error("Invalid hexString");
+        return;
+    }
+    var bytes = new Uint8Array(hexString.length / 2);
+    for (var i = 0; i < hexString.length; i += 2) {
+        bytes[i / 2] = parseInt(hexString.substr(i, 2), 16);
+    }
+    return bytes;
+}
+
+const staticHash = "0xebefff5d04586f1d5ba0d052d1a06f2535c5dd92be22c289295442b1048fe872"
+const staticHashBytes = hexToUint8Array(staticHash.slice(2));
+
+const dynHash = "0x4123f2d81428f7090218f975b941122f3797aeb8f97bf7d1ef6e87491c920a5c"
+const dynHashBytes = hexToUint8Array(dynHash.slice(2));
+
 if (WebAssembly) {
     if (WebAssembly && !WebAssembly.instantiateStreaming) { // polyfill
         WebAssembly.instantiateStreaming = async (resp, importObject) => {
@@ -9,6 +27,10 @@ if (WebAssembly) {
     WebAssembly.instantiateStreaming(fetch("main.wasm"), go.importObject).then((result) => {
         console.log("WebAssembly module loaded");
         go.run(result.instance);
+        const api = window.NesAPI();
+        console.log("NesAPI loaded", api);
+        api.start();
+        api.setCartridge(staticHashBytes, dynHashBytes);
     }).catch((err) => {
         console.error(err);
     });
--- wasm/main.go
+++ wasm/main.go
@@ -1,24 +1,35 @@
 package main
 
 import (
+	"bytes"
+	"compress/gzip"
 	_ "embed"
 	"fmt"
+	"io/ioutil"
 	"syscall/js"
 	"time"
 
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
 	"github.com/fogleman/nes/nes"
 )
 
 //go:embed state/mario.static
-var staticData []byte
+var marioStaticData []byte
 
 //go:embed state/mario.dyn
-var dynamicData []byte
+var marioDynData []byte
 
 const (
 	NES_WIDTH  = 256
 	NES_HEIGHT = 240
 )
+
+// TODO: proper cache
+var preimageCache = map[common.Hash][]byte{
+	hashStaticState(marioStaticData): marioStaticData,
+	hashDynState(marioDynData):       marioDynData,
+}
 
 func main() {
 
@@ -26,67 +37,175 @@ func main() {
 
 	kb := NewKeyboard()
 	renderer := NewRenderer()
-	pauser := NewPauser()
+	api := NewAPI()
+	governor := NewMovingAverage(15)
 
-	nes, err := nes.NewHeadlessConsole(staticData, dynamicData)
+	var staticData []byte
+	var dynData []byte
+
+	machine, err := nes.NewHeadlessConsole(marioStaticData, marioDynData)
 	if err != nil {
 		panic(err)
 	}
 
 	spf := time.Second / 60
+	speed := 1.0
+
+	<-api.startChan
+	fmt.Println("[wasm] Starting machine")
+
 	ticker := time.NewTicker(spf)
 	defer ticker.Stop()
 
-	for range ticker.C {
-		pauser.wait()
-		controller := kb.getController()
-		nes.Controller1.SetButtons(controller)
-		// startTime := time.Now()
-		nes.StepSeconds(spf.Seconds())
-		// fmt.Println("[wasm] Stepped", spf, "in", time.Since(startTime))
-		// startTime = time.Now()
-		renderer.renderImage(nes.Buffer().Pix)
-		// fmt.Println("[wasm] Renderer in", time.Since(startTime))
-	}
-}
-
-type pauser struct {
-	pauseChan   chan struct{}
-	unpauseChan chan struct{}
-}
-
-func NewPauser() *pauser {
-	p := &pauser{
-		pauseChan:   make(chan struct{}),
-		unpauseChan: make(chan struct{}),
-	}
-	window := js.Global().Get("window")
-	window.Call("addEventListener", "keyup", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
-		switch args[0].Get("code").String() {
-		case "KeyP":
-			p.Pause()
-		case "KeyO":
-			p.Unpause()
+	for {
+		select {
+		case <-api.pauseChan:
+			fmt.Println("[wasm] Paused")
+			<-api.pauseChan
+			fmt.Println("[wasm] Unpaused")
+		case newSpeed := <-api.speedChan:
+			speed = newSpeed
+			fmt.Println("[wasm] Speed changed to", newSpeed)
+		case preimage := <-api.preimageChan:
+			preimageCache[preimage.hash] = preimage.data
+			fmt.Println("[wasm] Received preimage", preimage.hash)
+		case cartridge := <-api.cartridgeChan:
+			_staticData, ok := preimageCache[cartridge.static]
+			if !ok {
+				fmt.Println("[wasm] Static preimage not found")
+				continue
+			}
+			_dynData, ok := preimageCache[cartridge.dyn]
+			if !ok {
+				fmt.Println("[wasm] Dynamic preimage not found")
+				continue
+			}
+			staticData = _staticData
+			dynData = _dynData
+			fmt.Println("[wasm] Inserted cartridge", cartridge.static, cartridge.dyn)
+		case <-ticker.C:
+			startTime := time.Now()
+			// fmt.Println("[wasm] Tick")
+			if staticData == nil || dynData == nil {
+				fmt.Println("[wasm] No cartridge")
+				continue
+			}
+			steps := int(speed * spf.Seconds() * nes.CPUFrequency)
+			controller := kb.getController()
+			machine.Controller1.SetButtons(controller)
+			for i := 0; i < steps; i++ {
+				machine.Step()
+			}
+			renderer.renderImage(machine.Buffer().Pix)
+			elapsedTime := time.Since(startTime)
+			avgMs := governor.Add(float64(elapsedTime.Milliseconds()))
+			spfMs := float64(spf.Milliseconds())
+			if avgMs > spfMs*0.875 {
+				newSpeed := speed * 0.99
+				speed = newSpeed
+			} else if speed < 1.0 && avgMs < spfMs*0.875 {
+				newSpeed := speed * 1.005
+				if newSpeed > 1.0 {
+					newSpeed = 1.0
+				}
+				speed = newSpeed
+			}
 		}
-		return nil
-	}))
-	return p
-}
-
-func (p *pauser) wait() {
-	select {
-	case <-p.pauseChan:
-		<-p.unpauseChan
-	default:
 	}
 }
 
-func (p *pauser) Pause() {
-	p.pauseChan <- struct{}{}
+type preimage struct {
+	hash common.Hash
+	data []byte
 }
 
-func (p *pauser) Unpause() {
-	p.unpauseChan <- struct{}{}
+type cartridge struct {
+	static common.Hash
+	dyn    common.Hash
+}
+
+type nesApi struct {
+	startChan     chan struct{}
+	pauseChan     chan struct{}
+	speedChan     chan float64
+	preimageChan  chan preimage
+	cartridgeChan chan cartridge
+}
+
+func NewAPI() *nesApi {
+	a := &nesApi{
+		startChan:     make(chan struct{}),
+		pauseChan:     make(chan struct{}),
+		speedChan:     make(chan float64),
+		preimageChan:  make(chan preimage),
+		cartridgeChan: make(chan cartridge),
+	}
+	js.Global().Set("NesAPI", js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+		return map[string]interface{}{
+			"start": js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+				a.start()
+				return nil
+			}),
+			"togglePause": js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+				a.togglePause()
+				return nil
+			}),
+			"setSpeed": js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+				a.setSpeed(args[0].Float())
+				return nil
+			}),
+			"setPreimage": js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+				jsHash := args[0]
+				jsData := args[1]
+
+				hash := make([]byte, jsHash.Get("byteLength").Int())
+				data := make([]byte, jsData.Get("byteLength").Int())
+
+				js.CopyBytesToGo(hash, jsHash)
+				js.CopyBytesToGo(data, jsData)
+
+				hashInGo := common.BytesToHash(hash)
+				a.setPreimage(hashInGo, data)
+				return nil
+			}),
+			"setCartridge": js.FuncOf(func(this js.Value, args []js.Value) interface{} {
+				jsStatic := args[0]
+				jsDyn := args[1]
+
+				static := make([]byte, jsStatic.Get("byteLength").Int())
+				dyn := make([]byte, jsDyn.Get("byteLength").Int())
+
+				js.CopyBytesToGo(static, jsStatic)
+				js.CopyBytesToGo(dyn, jsDyn)
+
+				staticInGo := common.BytesToHash(static)
+				dynInGo := common.BytesToHash(dyn)
+				a.setCartridge(staticInGo, dynInGo)
+				return nil
+			}),
+		}
+	}))
+	return a
+}
+
+func (a *nesApi) start() {
+	a.startChan <- struct{}{}
+}
+
+func (a *nesApi) togglePause() {
+	a.pauseChan <- struct{}{}
+}
+
+func (a *nesApi) setSpeed(speed float64) {
+	a.speedChan <- speed
+}
+
+func (a *nesApi) setPreimage(hash common.Hash, data []byte) {
+	a.preimageChan <- preimage{hash, data}
+}
+
+func (a *nesApi) setCartridge(static, dyn common.Hash) {
+	a.cartridgeChan <- cartridge{static, dyn}
 }
 
 type renderer struct {
@@ -136,6 +255,8 @@ func (r *renderer) renderImage(rawImageData []uint8) {
 	r.context.Call("putImageData", r.imageData, 0, 0)
 }
 
+// TODO: set controller t/ API [?]
+
 type keyboard struct {
 	keyStates map[string]bool
 }
@@ -173,4 +294,63 @@ func (kb *keyboard) getController() [8]bool {
 		kb.isPressed("ArrowLeft"),
 		kb.isPressed("ArrowRight"),
 	}
+}
+
+func hashStaticState(data []byte) common.Hash {
+	zip, err := compress(data)
+	if err != nil {
+		panic(err)
+	}
+	hash := crypto.Keccak256Hash(zip)
+	return hash
+}
+
+func hashDynState(data []byte) common.Hash {
+	// TODO: trie
+	hash := crypto.Keccak256Hash(data)
+	return hash
+}
+
+func compress(data []byte) ([]byte, error) {
+	var buffer bytes.Buffer
+	gz := gzip.NewWriter(&buffer)
+	if _, err := gz.Write(data); err != nil {
+		return nil, err
+	}
+	if err := gz.Close(); err != nil {
+		return nil, err
+	}
+	return buffer.Bytes(), nil
+}
+
+func decompress(data []byte) ([]byte, error) {
+	gz, err := gzip.NewReader(bytes.NewBuffer(data))
+	if err != nil {
+		return nil, err
+	}
+	defer gz.Close()
+	return ioutil.ReadAll(gz)
+}
+
+type MovingAverage struct {
+	size  int
+	sum   float64
+	queue []float64
+}
+
+func NewMovingAverage(size int) *MovingAverage {
+	return &MovingAverage{
+		size:  size,
+		queue: make([]float64, 0, size),
+	}
+}
+
+func (ma *MovingAverage) Add(value float64) float64 {
+	if len(ma.queue) >= ma.size {
+		ma.sum -= ma.queue[0]
+		ma.queue = ma.queue[1:]
+	}
+	ma.queue = append(ma.queue, value)
+	ma.sum += value
+	return ma.sum / float64(len(ma.queue))
 }

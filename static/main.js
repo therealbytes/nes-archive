11c7cc355e939d4ff39dfdc509cf4a6e66d482d5
bytes <therealbyt3s@gmail.com>
2023-05-22 14:16:34 +0200

    fix: hex2str

--- static/main.js
+++ static/main.js
@@ -3,6 +3,9 @@ function hexToUint8Array(hexString) {
         console.error("Invalid hexString");
         return;
     }
+    if (hexString.startsWith("0x")) {
+        hexString = hexString.slice(2);
+    }
     var bytes = new Uint8Array(hexString.length / 2);
     for (var i = 0; i < hexString.length; i += 2) {
         bytes[i / 2] = parseInt(hexString.substr(i, 2), 16);
@@ -11,10 +14,13 @@ function hexToUint8Array(hexString) {
 }
 
 const staticHash = "0xebefff5d04586f1d5ba0d052d1a06f2535c5dd92be22c289295442b1048fe872"
-const staticHashBytes = hexToUint8Array(staticHash.slice(2));
+const staticHashBytes = hexToUint8Array(staticHash);
 
 const dynHash = "0x4123f2d81428f7090218f975b941122f3797aeb8f97bf7d1ef6e87491c920a5c"
-const dynHashBytes = hexToUint8Array(dynHash.slice(2));
+const dynHashBytes = hexToUint8Array(dynHash);
+
+console.log("staticHashBytes", staticHashBytes, staticHashBytes.length);
+console.log("dynHashBytes", dynHashBytes, dynHashBytes.length);
 
 if (WebAssembly) {
     if (WebAssembly && !WebAssembly.instantiateStreaming) { // polyfill
